693bd0bf627cac98b15240f5b3113a94120e5c27
i5ting <i5ting@126.com>
2016-06-12 08:38:05 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ var program = require('commander');
 program
   .version('0.0.1')
   .option('-i, --init', 'init default .travis.yml')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-b, --badge', 'get badge for README.md')
   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
   .parse(process.argv);
  
 
 if (program.init) {
-
   fs.createReadStream(file_path + '/travis.yml').pipe(fs.createWriteStream(current_path + '/.travis.yml'))
   return console.log('init complete!')
+}
+
+if (program.badge) {
+  var cfg = require('get-git-info')(current_path);
+  // console.log(cfg)
+  //{ user: 'i5ting', project: 'get-git-info' }
+  return console.log('[![Build](https://travis-ci.org/' + cfg.user + '/' + cfg.project + '.svg?branch=master)](https://travis-ci.org/' + cfg.user + '/' + cfg.project + ')')
 }
 
 
