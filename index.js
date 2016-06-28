5d9c9dacfce2787fb3387b738b6c46b31390d0b2
i5ting <i5ting@126.com>
2016-06-28 23:16:22 +0800

    lazycommmit, please ignore

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ program
   .version(require('./package.json').version)
   .option('-i, --init', 'init default .travis.yml')
   .option('-b, --badge', 'get badge for README.md')
+  .option('-n, --npm', 'get npm badge .travis.yml')
+  .option('-g, --gitter', 'get gitter badge for README.md')
   .option('-c, --codecov', 'get codecov badge for README.md')
   .parse(process.argv);
  
@@ -34,6 +36,20 @@ if (program.codecov) {
   // console.log(cfg)
   //{ user: 'i5ting', project: 'get-git-info' }
    return console.log('[![codecov.io](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '/coverage.svg?branch=master)](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '?branch=master)')
+}
+
+if (program.npm) {
+  var cfg = require('get-git-info')(current_path);
+  // console.log(cfg)
+  //{ user: 'i5ting', project: 'get-git-info' }
+   return console.log('[![NPM version](https://img.shields.io/npm/v/' + cfg.project + '.svg?style=flat-square)](https://www.npmjs.com/package/' + cfg.project + ')')
+}
+
+if (program.gitter) {
+  var cfg = require('get-git-info')(current_path);
+  // console.log(cfg)
+  //{ user: 'i5ting', project: 'get-git-info' }
+   return console.log('[![gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/' + cfg.user + '/' + cfg.project + '?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)')
 }
 
 
