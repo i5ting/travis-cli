e58a8261e3ab141158dd65dd0f95868a15b97ab1
i5ting <i5ting@126.com>
2016-06-29 10:17:02 +0800

    refact with standard

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
 var fs = require('fs')
-var file_path = __dirname;
-var current_path = process.cwd();
+var filePath = __dirname
+var currentPath = process.cwd()
+var cfg = require('get-git-info')(currentPath)
 /**
  * Module dependencies.
  */
 
-var program = require('commander');
+var program = require('commander')
 
 program
   .version(require('./package.json').version)
@@ -17,51 +18,34 @@ program
   .option('-g, --gitter', 'get gitter badge for README.md')
   .option('-t, --travis', 'get travis badge for README.md')
   .option('-c, --codecov', 'get codecov badge for README.md')
-  .parse(process.argv);
- 
+  .parse(process.argv)
 
 if (program.init) {
-  fs.createReadStream(file_path + '/travis.yml').pipe(fs.createWriteStream(current_path + '/.travis.yml'))
-  return console.log('init complete!')
+  fs.createReadStream(filePath + '/travis.yml').pipe(fs.createWriteStream(currentPath + '/.travis.yml'))
+  console.log('init complete!')
 }
 
 if (program.badge) {
-  var cfg = require('get-git-info')(current_path);
-  // console.log(cfg)
-  //{ user: 'i5ting', project: 'get-git-info' }
   console.log('[![gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/' + cfg.user + '/' + cfg.project + '?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)')
   console.log('[![NPM version](https://img.shields.io/npm/v/' + cfg.project + '.svg?style=flat-square)](https://www.npmjs.com/package/' + cfg.project + ')')
   console.log('[![Build](https://travis-ci.org/' + cfg.user + '/' + cfg.project + '.svg?branch=master)](https://travis-ci.org/' + cfg.user + '/' + cfg.project + ')')
   console.log('[![codecov.io](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '/coverage.svg?branch=master)](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '?branch=master)')
-  return;
 }
 
 if (program.codecov) {
-  var cfg = require('get-git-info')(current_path);
-  // console.log(cfg)
-  //{ user: 'i5ting', project: 'get-git-info' }
-   return console.log('[![codecov.io](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '/coverage.svg?branch=master)](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '?branch=master)')
+  console.log('[![codecov.io](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '/coverage.svg?branch=master)](https://codecov.io/github/' + cfg.user + '/' + cfg.project + '?branch=master)')
 }
 
 if (program.npm) {
-  var cfg = require('get-git-info')(current_path);
-  // console.log(cfg)
-  //{ user: 'i5ting', project: 'get-git-info' }
-   return console.log('[![NPM version](https://img.shields.io/npm/v/' + cfg.project + '.svg?style=flat-square)](https://www.npmjs.com/package/' + cfg.project + ')')
+  console.log('[![NPM version](https://img.shields.io/npm/v/' + cfg.project + '.svg?style=flat-square)](https://www.npmjs.com/package/' + cfg.project + ')')
 }
 
 if (program.gitter) {
-  var cfg = require('get-git-info')(current_path);
-  // console.log(cfg)
-  //{ user: 'i5ting', project: 'get-git-info' }
-   return console.log('[![gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/' + cfg.user + '/' + cfg.project + '?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)')
+  console.log('[![gitter](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/' + cfg.user + '/' + cfg.project + '?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)')
 }
 
 if (program.travis) {
-  var cfg = require('get-git-info')(current_path);
-  // console.log(cfg)
-  //{ user: 'i5ting', project: 'get-git-info' }
-   return console.log('[![Build](https://travis-ci.org/' + cfg.user + '/' + cfg.project + '.svg?branch=master)](https://travis-ci.org/' + cfg.user + '/' + cfg.project + ')')
+  console.log('[![Build](https://travis-ci.org/' + cfg.user + '/' + cfg.project + '.svg?branch=master)](https://travis-ci.org/' + cfg.user + '/' + cfg.project + ')')
 }
 
-console.log('Usages: travis -i or travis -b')
+// console.log('Usages: travis -i or travis -b')
